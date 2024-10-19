ed7a878ce2916598ea547b8a523a65beb13063dc
Dang Quoc Trinh <quoctrinh1357@gmail.com>
2024-10-19 15:10:00 +0700

    update

---
+++ js/javascript.js
@@ -0,0 +1,29 @@
+var loader = document.getElementById("loader");
+window.addEventListener('load', function (load) {
+    window.removeEventListener('load', load, false);
+    setTimeout(function () { loader.style.display = 'none' }, 3000);
+}, false);
+
+$(document).ready(function () {
+    $('#bars').click(function () {
+        $('.menu').slideToggle('show');
+    });
+
+    $('.menu').click(function () {
+        if ($(window).width() <= 992) {
+            $('.menu').hide();
+        }
+    });
+
+    $(window).resize(function () {
+        if ($(window).width() > 992) {
+            $('.menu').removeAttr('style');
+        } else {
+            $('.menu').hide();
+        }
+    });
+
+    if ($(window).width() > 992) {
+        $('.menu').removeAttr('style');
+    }
+})
